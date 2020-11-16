2f12083588405d9c06d45c0cbb1027c4d94956c1
Howuhh <hsehowuhh@gmail.com>
2020-11-16 18:39:40 +0300

    hw9 and contest3 b

--- contest3/b.py
+++ contest3/b.py
@@ -1,46 +1,42 @@
 import sys
 
+from math import inf
 from collections import deque
 
-def bfs(root, exits, adj):
+def bfs(exits, adj):
+    p = list(range(len(adj)))
+    dist = [inf] * len(adj)
+     
     visited = [False] * len(adj)
-    dist = [0] * len(adj) 
     
-    queue = deque([root])
-    visited[root] = True
-    
+    queue = deque(exits)
+    for ex in exits:
+        visited[ex] = True
+        dist[ex] = 0
+        
     while queue:
         node = queue.pop()
         
         for neigh in adj[node]:
             if not visited[neigh]:
+                p[neigh] = p[node]
                 visited[neigh] = True
                 dist[neigh] = dist[node] + 1
-                
-                if neigh in exits:
-                    return (dist[neigh], neigh + 1)
-                
-def min_dist_exit(exits, adj):
-    dist = [0] * len(adj)
-    ex = list(range(1, len(adj) + 1))
+                queue.appendleft(neigh)
+            else:
+                if dist[node] + 1 < dist[neigh] or (dist[node] + 1 == dist[neigh] and p[node] < p[neigh]):
+                    dist[neigh] = dist[node] + 1
+                    p[neigh] = p[node]
+                    
+    return dist, [i + 1 for i in p]
 
-    for i in range(len(adj)):
-        if i not in exits:
-            min_dist = bfs(i, exits, adj)
-            dist[i] = min_dist[0]
-            ex[i] = min_dist[1]
-        
-    print(*dist)
-    print(*ex)
 
 def main():
     N = int(sys.stdin.readline())
     K = int(sys.stdin.readline())
     
-    exits = set()
-    for _ in range(K):
-        exits.add(int(sys.stdin.readline()) - 1)
-        
+    exits = [int(i) - 1 for i in sys.stdin.readline().split()]
+
     M = int(sys.stdin.readline())
     adj = [[] for _ in range(N)]
 
@@ -49,7 +45,9 @@ def main():
         adj[a - 1].append(b - 1)
         adj[b - 1].append(a - 1)
     
-    min_dist_exit(exits, adj)
+    dist, p = bfs(exits, adj)
+    print(*dist)
+    print(*p)
 
 
 if __name__ == "__main__":
